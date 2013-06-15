47066fcc11fff4947a438527425b447900954f80
Gopala KODURI <gkoduri@devaraya.s.upf.edu>
2013-06-15 11:41:19 +0200

    Fixed bugs with NN

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,7 +4,7 @@
 #mail: gopala.koduri@gmail.com
 
 from __future__ import division
-from scipy.optimize import fmin_bfgs, fmin_cg
+from scipy.optimize import fmin_bfgs, fmin_cg, fmin_ncg
 import numpy as np
 
 count = 0
@@ -62,7 +62,7 @@ def cost(nnParams, hiddenLayerSizes,\
         numValues = curLayerSize * (prevLayerSize+1)
         endIndex = startIndex+numValues
         theta = nnParams[startIndex:endIndex]
-        theta = theta.reshape(curLayerSize, prevLayerSize+1)
+        theta = theta.reshape(curLayerSize, prevLayerSize+1, order="F")
         theta = np.matrix(theta)
         allTheta.append(theta)
         startIndex=endIndex
@@ -92,10 +92,10 @@ def cost(nnParams, hiddenLayerSizes,\
     #last activation corresponds to the prediction values
     predictions = activation
 
-    #calculate cost and gradient!
+    #calculate cost
     #first, transform each y into suitable vector of np.zeros and np.ones
     yMatrix = []
-    for i in y.A[0]:
+    for i in y.reshape(y.size).A[0]:
         tmp = np.linspace(0, numLabels-1, numLabels)
         tmp = tmp == i
         tmp = tmp*1
@@ -105,18 +105,19 @@ def cost(nnParams, hiddenLayerSizes,\
     #compute cost
     #numpy.np.multiply returns element-wise multiplication of matrices and np.arrays
     J = np.multiply(yMatrix, np.log(predictions)) + np.multiply(1-yMatrix, np.log(1-predictions))
-    J = -sum(J.reshape(J.size, 1))/numSamples
+    J = J.reshape(J.size, order="F").A[0]
+    J = -sum(J)/numSamples
 
     #regularize the cost
     flatTheta = []
     for t in allTheta:
-        tmp = np.reshape(t, t.size).A[0]
+        t = t[:, 1:]
+        tmp = np.reshape(t, t.size, order="F").A[0]
         flatTheta.extend(tmp)
 
     J = J + _lambda*sum(np.multiply(flatTheta, flatTheta))/(2*numSamples)
-
-    print count, J.A[0][0]
-    return J.A[0][0]
+    print count, J
+    return J
 
 def derivatives(nnParams, hiddenLayerSizes,\
          numLabels, X, y, _lambda):
@@ -149,7 +150,7 @@ def derivatives(nnParams, hiddenLayerSizes,\
         numValues = curLayerSize * (prevLayerSize+1)
         endIndex = startIndex+numValues
         theta = nnParams[startIndex:endIndex]
-        theta = theta.reshape(curLayerSize, prevLayerSize+1)
+        theta = theta.reshape(curLayerSize, prevLayerSize+1, order="F")
         theta = np.matrix(theta)
         allTheta.append(theta)
         startIndex=endIndex
@@ -160,6 +161,7 @@ def derivatives(nnParams, hiddenLayerSizes,\
     #compute activations using thetas and inputs
     allActivations = []
     allZs = []
+    allZs.append(np.matrix([])) #dummy matrix to keep indexing easy for backprop
     #use numpy.np.insert to add column of np.ones!
     #np.insert(np.matrix/arr, indexToInsertBefore, axis=0(row)/1(column))
     layerInput = np.insert(X, 0, np.ones(numSamples), axis=1)
@@ -176,7 +178,7 @@ def derivatives(nnParams, hiddenLayerSizes,\
     #calculate the gradient!
     #first, transform each y into suitable vector of np.zeros and np.ones
     yMatrix = []
-    for i in y.A[0]:
+    for i in y.reshape(y.size).A[0]:
         tmp = np.linspace(0, numLabels-1, numLabels)
         tmp = tmp == i
         tmp = tmp*1
@@ -191,30 +193,34 @@ def derivatives(nnParams, hiddenLayerSizes,\
     #delta for output layer is just activations-y, and from it the other deltas are computed
     delta = allActivations[-1]-yMatrix
     deltaNextLayer = delta
-    for i in xrange(numHiddenLayers+1):
-        layerNum = numHiddenLayers-i-1
-        activations = allActivations[layerNum+1]
+
+    #Imagine input layer is layer0, hidden layers 1,2.. and output layer n-1.
+    #allActivations[0] are activations of first layer
+    #allZs[0] is allActivations[0]*allTheta[0]
+    _range = np.array(range(numHiddenLayers+1))+1
+    for layerNum in reversed(_range):
+        activations = allActivations[layerNum-1]
         activations = np.insert(activations, 0, np.ones(activations.shape[0]), axis=1)
         derivative = deltaNextLayer.T*activations
         derivative = derivative/numSamples
-        #print activations.shape, deltaNextLayer.shape, derivative.shape
 
         #add regularization
-        theta = allTheta[layerNum+1]
+        theta = allTheta[layerNum-1]
         firstCol = derivative[:, 0]
-        firstCol = firstCol.reshape(1, firstCol.size)
+        firstCol = firstCol.reshape(firstCol.size, order="F")
         derivative = derivative[:, 1:] + (_lambda/numSamples)*theta[:, 1:]
         derivative = np.insert(derivative, 0, firstCol, axis=1)
 
-        allDerivatives[layerNum] = derivative
+        allDerivatives[layerNum-1] = derivative
 
         #compute delta of this layer and move on to previous layer
-        if i == numHiddenLayers: break
+        #and remember we don't need delta for input layer, so...
+        if layerNum == 1:
+            break
 
         delta = deltaNextLayer*theta
         delta = delta[:, 1:]
-        delta = np.multiply(delta, sigmoidGradient(allZs[layerNum]))
-        #print theta.shape, delta.shape
+        delta = np.multiply(delta, sigmoidGradient(allZs[layerNum-1]))
 
         #allDeltas[layerNum] = delta
         deltaNextLayer = delta
@@ -222,8 +228,8 @@ def derivatives(nnParams, hiddenLayerSizes,\
 
     #unroll partial derivative terms
     grad = np.array([])
-    for D in reversed(allDerivatives):
-        grad = np.append(grad, D.reshape(D.size).A[0], axis=0)
+    for D in allDerivatives:
+        grad = np.append(grad, D.reshape(D.size, order="F").A[0], axis=0)
 
     return grad
 
@@ -237,7 +243,7 @@ def predict(allTheta, X):
     numSamples = X.shape[0]
     _input = np.insert(X, 0, np.ones(numSamples), axis=1)
     for theta in allTheta:
-        result = sigmoid(_input*theta)
+        result = sigmoid(_input*theta.T)
         _input = np.insert(result, 0, np.ones(numSamples), axis=1)
 
     predictions = np.argmax(result, axis=1)
@@ -264,8 +270,8 @@ def train(X, y, hiddenLayerSizes,\
                                     _lambda)
     funcDerivativeHandle = lambda theta: derivatives(theta, hiddenLayerSizes, numLabels, X, y,\
                                     _lambda)
-    res = fmin_bfgs(funcHandle, initialTheta, fprime=funcDerivativeHandle,\
-                    maxiter=10)
+    res = fmin_cg(funcHandle, initialTheta, fprime=funcDerivativeHandle,\
+                    maxiter=50)
 
     #roll back theta values and return an allTheta!
     allTheta = [] #np.array of theta matrices
@@ -282,13 +288,25 @@ def train(X, y, hiddenLayerSizes,\
         numValues = curLayerSize * (prevLayerSize+1)
         endIndex = startIndex+numValues
         theta = res[startIndex:endIndex]
-        theta = theta.reshape(prevLayerSize+1, curLayerSize)
+        theta = theta.reshape(curLayerSize, prevLayerSize+1, order="F")
         theta = np.matrix(theta)
         allTheta.append(theta)
         startIndex=endIndex
         prevLayerSize = curLayerSize
 
     return allTheta
+
+def numGrad(theta, hiddenLayerSizes, numLabels, X, y, _lambda):
+    numgrad = np.zeros(theta.size);
+    perturb = np.zeros(theta.size);
+    e = 1e-4;
+    for p in xrange(theta.size):
+        perturb[p] = e;
+        loss1 = cost(theta - perturb, hiddenLayerSizes, numLabels, X, y, _lambda);
+        loss2 = cost(theta + perturb, hiddenLayerSizes, numLabels, X, y, _lambda);
+        numgrad[p] = (loss2 - loss1) / (2*e);
+        perturb[p] = 0;
+    return numgrad
 
 def check(initialTheta, X, y, hiddenLayerSizes, numLabels, _lambda):
 #    inputLayerSize = X.shape[1]
@@ -303,6 +321,7 @@ def check(initialTheta, X, y, hiddenLayerSizes, numLabels, _lambda):
     
     c = cost(initialTheta, hiddenLayerSizes, numLabels, X, y, _lambda)
     d = derivatives(initialTheta, hiddenLayerSizes, numLabels, X, y, _lambda)
+    #nd = numGrad(initialTheta, hiddenLayerSizes, numLabels, X, y, _lambda)
 
     return [c,d]
 
